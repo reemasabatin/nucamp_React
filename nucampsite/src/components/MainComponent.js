8f9bb5e5f744d8cf04332e8716531b10a5022661
Reema Sabatin <rmsabatin@up.edu.ph>
2022-03-12 08:02:02 +0800

    Header and Footer

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
+import Header from './FooterComponent';
+import Footer from './HeaderComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 
@@ -20,13 +21,10 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">NuCamp</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
             </div>
         );
     };
